2f67db24e23cca58a84447fb9a3d908fe8b22232
haniye <40136586+haniye94@users.noreply.github.com>
2020-04-29 17:58:37 +0430

    android pull test

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //this is android push test
         //this pull test
+        //this is android pull test
     }
 }
